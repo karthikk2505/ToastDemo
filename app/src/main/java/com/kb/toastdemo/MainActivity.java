1060af0a3afabd61477734ce292b000fe14ab7c1
karthik2505 <karthickbharathi111@gmail.com>
2020-04-03 13:30:14 +0530

    ff

---
+++ app/src/main/java/com/kb/toastdemo/MainActivity.java
@@ -0,0 +1,16 @@
+package com.kb.toastdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(getApplicationContext(),"Hello App Toast", Toast.LENGTH_SHORT).show();
+    }
+}
